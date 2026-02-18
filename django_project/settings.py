5308e3f2411d8d85cfe2163043083fb7a9b7f872
MamporiaG <gurammamphoria@gmail.com>
2026-02-18 22:19:19 +0000

    added render host

--- django_project/settings.py
+++ django_project/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["drf-todo-8i52.onrender.com",]
 
 
 # Application definition
